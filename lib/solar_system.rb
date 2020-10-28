87c5593b4df6bb4d2974610baf78c64c48946664
Nicole B <nicole@xadi.io>
2020-10-28 16:10:16 -0400

    solar system tests + class skeleton

--- lib/solar_system.rb
+++ lib/solar_system.rb
@@ -1,8 +1,33 @@
+# SolarSystem description object
 class SolarSystem
+  attr_reader :score, :current_owner, :status
+
+  def initialize(json); end
+
+  def to_i; end
+
+  def to_s; end
+
+  def pct; end
+
+  def plexes; end
+
+  def -(other); end
 end
 
+# Score object to describe (vp/vp_max) in varying methods
 class SystemScore
-end
+  def initialize(vp_current, vp_max = 3000); end
 
-class SystemDelta
+  def to_i; end
+
+  def to_f; end
+
+  def to_r; end
+
+  def to_s; end
+
+  def pct; end
+
+  def -(other); end
 end
