3b530e28c6ac643ae9d2c43397e15b4e0c3b6dbf
Nicole B <nicole@faenre.dev>
2020-11-11 19:32:17 -0500

    new query for system histories

---
+++ sql/query_system_history.sql
@@ -0,0 +1,11 @@
+SELECT f.solar_system_id,
+       f.victory_points,
+       f.victory_points_threshold,
+       f.occupier_faction_id
+  FROM fw_system_statuses AS f
+ WHERE f.id IN (SELECT r.id
+                  FROM esi_responses AS r
+                 WHERE r.code BETWEEN 200 AND 399
+                 GROUP BY r.last_modified
+                 ORDER BY r.id DESC
+                 LIMIT %s);
