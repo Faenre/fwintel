415e807c95449919759a09b8abbf41f2c6e24cb9
Nicole B <nicole@faenre.dev>
2020-11-22 07:46:58 -0500

    added names to query

--- sql/query_system_history.sql
+++ sql/query_system_history.sql
@@ -1,8 +1,11 @@
 SELECT f.solar_system_id,
+       e.name,
        f.victory_points,
        f.victory_points_threshold,
        f.occupier_faction_id
   FROM fw_system_statuses AS f
+       JOIN eve_universe AS e
+       ON f.solar_system_id = e.id
  WHERE f.id IN (SELECT r.id
                   FROM esi_responses AS r
                  WHERE r.code BETWEEN 200 AND 399
