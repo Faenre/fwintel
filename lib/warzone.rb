fbd06ed89df92a081aedc66b54e0b923adbf82db
Nicole B <nicole@faenre.dev>
2021-01-21 12:10:40 -0500

    warzone initial buildout

--- lib/warzone.rb
+++ lib/warzone.rb
@@ -1,19 +1,36 @@
-class WarzoneFactory
-  def self.from_esi(response); end
+require 'json'
+require './lib/solar_system'
 
-  def self.from_json(json); end
+class WarzoneFactory
+  def self.from_web(response)
+    from_json JSON.parse(response.body)
+  end
+
+  def self.from_json(json)
+    Warzone.new(json)
+  end
 end
 
 # Summary object to describe and navigate the contents of /fw/systems/
 class Warzone
-  include Enumerable
+  attr_reader :systems
 
-  def initialize(body, response_code, timestamp, expiry); end
+  def initialize(body)
+    @systems = body.map { |system| SolarSystemFactory.from_esi(system) }
+  end
 
-  def []; end
+  def [](key)
+    case key.class.to_s
+    when 'Integer'  then @systems.detect { |sys| sys.eve_id == key }
+    when 'String'   then @systems.detect { |sys| sys.eve_name == key }
+    end
+  end
 
-  def each; end
+  def tally_by_faction
+    @systems.map(&:owner).tally
+  end
 end
 
 class WarzoneDelta
+  def initialize; end
 end
