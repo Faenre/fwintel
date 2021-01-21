e62ebd71aeb1a4fbcd4112a5a0813e624cc1700b
Nicole B <nicole@faenre.dev>
2021-01-21 12:08:11 -0500

    adjusted sql connector with interface

---
+++ lib/external/postgres.rb
@@ -0,0 +1,11 @@
+require 'pg'
+
+class SQLConnector
+  def initialize(config)
+    @connection = PG::Connection.new(config)
+  end
+
+  def exec(statement, &:block)
+    @connection.exec statement, :block
+  end
+end
