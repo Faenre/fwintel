a28ebb920d7b0fe9adaa089180bfc39916c4954c
Nicole B <nicole@faenre.dev>
2021-01-21 12:10:20 -0500

    warzone and solar system tests

--- test/solar_system_test.rb
+++ test/solar_system_test.rb
@@ -1,8 +1,10 @@
 require 'bundler/setup'
-require 'JSON'
 
+require 'simplecov'
+SimpleCov.start
 require 'minitest/autorun'
 
+require 'json'
 require './lib/solar_system'
 
 # SolarSystem descriptor object tests
@@ -20,124 +22,25 @@ class SolarSystemTest < MiniTest::Test
 
   def setup
     @system_json = JSON.parse(SAMPLE_JSON)
-    @system = SolarSystem.new(@system_json)
+    @system = SolarSystemFactory.from_esi(@system_json)
   end
 
-  def test_attributes_have_getters
-    @system.score
-    @system.current_owner
-    @system.status
-    assert true
-  rescue NoMethodError
-    assert false
+  def test_to_i_returns_num_plexes
+    assert_equal(25, @system.to_i)
   end
 
-  def test_attributes_dont_have_setters
-    assert_raises(NoMethodError) { @system.score = 1 }
-    assert_raises(NoMethodError) { @system.current_owner = 2 }
-    assert_raises(NoMethodError) { @system.status = 3 }
-  end
+  # def test_to_json
+  #   answer = ''.concat(
+  #     '{"contested":"contested","occupier_faction_id":500003,',
+  #     '"owner_faction_id":500003,"solar_system_id":30002957,',
+  #     '"victory_points":500,"victory_points_threshold":3000}'
+  #   )
+  #   assert_equal answer, @system.to_json
+  # end
 
-  def test_attributes_are_initialized_correctly
-    assert_instance_of SystemScore, @system.score
-    assert_instance_of Symbol, @system.status
-    assert_instance_of Symbol, @system.owner
-  end
-
-  # rubocop:disable Style/NumericLiterals
-  def test_to_i
-    assert_equal(30002957, @system.to_i)
-  end
-  # rubocop:enable Style/NumericLiterals
-
-  def test_to_json
-    answer = ''.concat(
-      '{"contested":"contested","occupier_faction_id":500003,',
-      '"owner_faction_id":500003,"solar_system_id":30002957,',
-      '"victory_points":500,"victory_points_threshold":3000}'
-    )
-    assert_equal answer, @system.to_json
-  end
-
-  def test_score_returns_system_score
-    assert_instance_of(SystemScore, @system.score)
-  end
-
-  # should return score percent as string with formatting
-  def test_pct_includes_formatting
+  def test_to_pct_correct_formatting
     expected = '16.7%'
-    assert_equal(expected, @system.pct)
-  end
-
-  def teardown; end
-end
-
-# Tests for the system score, which turns '1260 victory points'
-# into meaningful figures
-class SystemScoreTest < MiniTest::Test
-  MAX = 3000
-  SAMPLE_SCORES = [
-    [0, MAX],
-    [480, MAX],
-    [520, MAX],
-    [1280, MAX],
-    [MAX, MAX],
-    [MAX + 20, MAX]
-  ].freeze
-
-  def setup
-    @scores = SAMPLE_SCORES.map { |vp, vpmax| SystemScore.new vp, vpmax }
-  end
-
-  # confirm these values don't have public accessors
-  def test_attribs_should_be_hidden
-    assert_raises(NoMethodError) { @scores.sample.vp }
-    assert_raises(NoMethodError) { @scores.sample.vp_max }
-    assert_raises(NoMethodError) { @scores.sample.get_vp }
-    assert_raises(NoMethodError) { @scores.sample.get_vp_max }
-  end
-
-  # confirm #to_i returns the number of plexes captured
-  def test_to_i
-    answers = [0, 24, 26, 34, 100, 101]
-
-    @scores.each_with_index do |score, idx|
-      assert_equal(answers[idx], score.to_i)
-    end
-  end
-
-  def test_to_r
-    answers = SAMPLE_SCORES.map { |vp, vp_max| Rational(vp, vp_max) }
-
-    @scores.each_with_index do |score, idx|
-      assert_equal(answers[idx], score.to_r)
-    end
-  end
-
-  # confirm #to_f returns a the percent as float, e.g. 1000vp = .333
-  def test_to_f
-    answers = SAMPLE_SCORES.map { |vp, vp_max| vp.fdiv vp_max }
-
-    @scores.each_with_index do |score, idx|
-      assert_equal(answers[idx], score.to_f)
-    end
-  end
-
-  # confirm #to_s returns the to_f value, as '33.3' syntax
-  def test_to_s
-    answers = ['0.0', '16.0', '17.3', '42.7', '100.0', '100.7']
-
-    @scores.each_with_index do |score, idx|
-      assert_equal(answers[idx], score.to_s)
-    end
-  end
-
-  def test_pct
-    answers = ['0.0%', '16.0%', '17.3%', '42.7%', '100.0%', '100.7%']
-
-    @scores.each_with_index do |score, idx|
-      assert_equal(answers[idx], score.to_s)
-    end
+    assert_equal(expected, @system.to_pct)
   end
 
   def teardown; end
--- test/warzone_test.rb
+++ test/warzone_test.rb
@@ -1,6 +1,9 @@
 require 'bundler/setup'
 
+require 'simplecov'
+SimpleCov.start
 require 'minitest/autorun'
+
 require 'json'
 
 require './lib/warzone'
@@ -11,64 +14,54 @@ SAMPLE_CONTENT = JSON.parse(File.open(SAMPLE_FILE).read).freeze
 # Test the warzone creation suite
 class WarzoneFactoryTest < MiniTest::Test
   def setup; end
+  def teardown; end
 
   def test_from_esi
+    skip
+
     sample_response = MockHTTP.new
     WarzoneFactory.from_esi(sample_response)
   end
 
-  def teardown; end
+  def test_from_json
+    skip
+
+    WarzoneFactory.from_json(SAMPLE_CONTENT)
+  end
 end
 
 # Test the warzone summary object
 class WarzoneTest < MiniTest::Test
   def setup
-    time_now = DateTime.now
-    time_future = DateTime.now + 5000
-    args = { body: CONTENT, response: 200,
-             timestamp: time_now, expiry: time_future }
-    @warzone = Warzone.new(*args)
+    @warzone = Warzone.new(SAMPLE_CONTENT)
   end
 
-  def test_attributes_have_getters
-    @warzone.systems
-    @warzone.timestamp
-    @warzone.next_update_at
-    assert true
-  rescue NoMethodError
-    assert false
-  end
-
-  def test_attributes_dont_have_setters
-    assert_raises(NoMethodError) { @warzone.systems = 1 }
-    assert_raises(NoMethodError) { @warzone.timestamp = 2 }
-    assert_raises(NoMethodError) { @warzone.next_update_at = 3 }
+  def test_warzone_initializes_properly
+    assert_instance_of Warzone, @warzone
   end
 
   def test_attributes_are_initialized_correctly
-    assert_instance_of Hash, @warzone.systems
-    assert_instance_of DateTime, @warzone.timestamp
-    assert_instance_of DateTime, @warzone.next_update_at
+    assert_instance_of Array, @warzone.systems
+    assert(@warzone.systems.all? { |sys| sys.is_a? SolarSystem })
   end
 
-  def test_subtraction_returns_delta
-    assert_instance_of WarzoneDelta, @warzone - @warzone
-  end
-
-  def test_expired
-    refute @warzone.expired?
-  end
-
-  # rubocop:disable Style/NumericLiterals
   def test_get_system_by_name
     system_tests = [['Tzvi', 30002957],
                     ['Murethand', 30005295],
                     ['Ladistier', 30004999]]
-    system_tests.each do |name, id|
-      assert_equal id, @warzone[name]
+    system_tests.each do |eve_name, id|
+      assert_equal id, @warzone[eve_name].eve_id
     end
   end
-  # rubocop:enable Style/NumericLiterals
+
+  def test_get_system_by_id
+    system_tests = [['Tzvi', 30002957],
+                    ['Murethand', 30005295],
+                    ['Ladistier', 30004999]]
+    system_tests.each do |eve_name, id|
+      assert_equal eve_name, @warzone[id].eve_name
+    end
+  end
 
   def teardown; end
 end
@@ -77,24 +70,4 @@ class WarzoneDeltaTest < MiniTest::Test
   def setup; end
 
   def teardown; end
-end
-
-# initialization stub
-class SolarSystem
-  def initialize(*_); end
-end
-
-# mock monkey-patch
-class MockHTTP
-  def body
-    SAMPLE_CONTENT
-  end
-
-  def code
-    200
-  end
-
-  def headers
-    { 'expires': 'Wed, 28 Oct 2020 21:38:10 GMT ' }
-  end
 end
